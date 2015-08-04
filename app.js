1ee84e3f57cf1d97af6143095e5d581d78d416ce
Giulian Luz <giulian@Giulians-MacBook-Pro.local>
2015-08-04 12:30:44 -0300

    Basic jscs and jshint rules and gulp enable

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+'use strict';
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,38 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
+'use strict';
+var gulp    = require('gulp');
+var notify  = require('gulp-notify');
+var jscs    = require('gulp-jscs');
+var jshint  = require('gulp-jshint');
+var exec    = require('child_process').exec;
+
+gulp.task('jscs', function() {
+  gulp.src('*.js')
+  .pipe(jscs())
+  .pipe(notify({
+    title: 'JSCS',
+    message: 'JSCS Passed. Let it fly!',
+  }));
+});
+
+gulp.task('lint', function() {
+  gulp.src('*.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail'))
+  .pipe(notify({
+    title: 'JSHint',
+    message: 'JSHint Passed. Let it fly!',
+  }));
+});
 
 gulp.task('default', function() {
   console.log('Hello world.');
 });
 
-
-gulp.task('server', function (cb) {
-  exec('node app.js', function (err, stdout, stderr) {
+gulp.task('server', function(cb) {
+  exec('node app.js', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
-})
+});
