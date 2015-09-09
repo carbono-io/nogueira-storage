9b21f317321b6ef92a50a009fd93d63121d14bfb
Renan Dias <renansdiaz@gmail.com>
2015-09-09 08:45:01 -0300

    Initial commit on develop

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 'use strict';
-var app = require('./app/app.js');
 
-app('Hi There!!!');
+var express    = require('express');
+var consign    = require('consign');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PORT = 13956;
+
+// Json parser for post data
+app.use(bodyParser.json());
+
+consign({cwd: 'app'})
+    .include('controllers')
+    .include('routes')
+    .into(app);
+
+var server = app.listen(PORT, function () {
+    console.log('Nogueira Storage listening at http://%s:%s',
+        server.address().address, server.address().port);
+});
